eb303a0f588523a405f7628dbcf53cc1ee3328ce
Shubh0260 <98557237+Shubh0260@users.noreply.github.com>
2022-03-27 06:30:49 -0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,26 @@
+var library = [{
+
+        author: 'The Road Ahead',
+        title: 'Bill Gates',
+        readingStatus: true
+    },
+    {
+        author: 'Walter Isaacson',
+        title: 'Steve Jobs',
+        readingStatus: true
+    },
+    {
+        author: 'Suzanne Collins',
+        title: 'Mockingjay: The Final Book of The Hunger Games',
+        readingStatus: false
+    }
+];
+
+for (var i = 0; i < library.length; i++) {
+    var book = "'" + library[i].title + "'" + ' by ' + library[i].author + ".";
+    if (library[i].readingStatus) {
+        console.log("Already read " + book);
+    } else {
+        console.log("You still need to read " + book);
+    }
+}
